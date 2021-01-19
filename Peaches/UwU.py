813fb0f688f2f58788398f2e3d67f2a2136e3644
Luna Sofia <nore0404@gmail.com>
2021-01-19 08:59:14 +0000

    Added comment descriptions

--- Peaches/UwU.py
+++ Peaches/UwU.py
@@ -1,9 +1,10 @@
+# Import libraries
 import os
 
-os.system('cmd /c "python get-pip.py"')
-os.system('cmd /c "pip install --upgrade pip"')
+# See if pip is already installed
+os.system('cmd /c "python get-pip.py"') # Check for pip installed
+os.system('cmd /c "pip install --upgrade pip"') # Upgrade or installed pip if not/it is installed
 
-os.system('cmd /c "pip3 install pynput"')
-print("Successfully installed pynput")
+os.system('cmd /c "pip3 install pynput"') # Install pynput library
 
-exec(open("info.py").read())
+exec(open("info.py").read()) # Open second file for execution of script
--- Peaches/info.py
+++ Peaches/info.py
@@ -1,11 +1,13 @@
+# Import libraries
 from pynput import mouse
 import webbrowser
 from time import sleep
 
+# Define on_click to open tab with youtube video
 def on_click (x, y, button, pressed):
     webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 listener1 = mouse.Listener(
     on_click = on_click)
-listener1.start()
+listener1.start() # Start input listener
 sleep(30)
-listener1.stop()
+listener1.stop() # Stop input listener
